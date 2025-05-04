30a2999f7aa739a560d2bb975aee13cf3479ac88
iliasbatas1 <ilyasbatas345@gmail.com>
2025-05-04 15:46:23 +0000

    adding Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:23.10.0
+
+# Set the working directory
+WORKDIR /app
+
+# Copy package files
+COPY package.json package-lock.json* ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application and .env file
+COPY . .
+
+RUN --mount=type=secret,id=envfile \
+    bash -c "export $(grep -v '^#' /run/secrets/envfile | xargs) && npm run build"
+
+# Start the application
+CMD ["npm", "start"]
--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -4,27 +4,25 @@ import React from 'react';
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from 'resend';
 
-// Initialize the Resend client (with your API key)
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: NextRequest) {
     try {
-        const { name, email,subject, message } = await req.json();
-        
+        const { name, email, subject, message } = await req.json();
+
+        // Initialise Resend DANS la fonction, là où process.env est dispo
+        const resend = new Resend(process.env.RESEND_API_KEY);
+
         // Cast ContactEmail as a JSX component function
         const emailHtml = render(
             React.createElement(ContactEmail as any, { name, email, message })
         );
-        const renderedHtml = await emailHtml;
-        
-        resend.emails.send({
+
+        await resend.emails.send({
             from: 'onboarding@resend.dev',
             to: 'tshensowgamer2004@gmail.com',
             subject: subject,
-            html: renderedHtml,
-          });
-        
-        
+            html: emailHtml,
+        });
+
         return NextResponse.json({ success: true });
     } catch (error) {
         return NextResponse.json(
